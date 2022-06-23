cc5b6fed2e04fcc0f3afabfb5f60c442b08131ad
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-06-23 23:47:27 +0800

    Task 13.6.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 const { Blog } = require('../models');
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  return next();
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll();
   console.log(JSON.stringify(blogs, null, 2));
@@ -18,18 +23,31 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedBlog = await Blog.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    console.log(deletedBlog.toJSON());
-    return res.json(deletedBlog);
-  } catch (error) {
-    return res.status(400).json({ error });
+router.get('/:id', blogFinder, async (req, res) => {
+  if (!req.blog) {
+    return res.status(404).end();
   }
+  console.log(req.blog.toJSON());
+  return res.json(req.blog);
+});
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (!req.blog) {
+    return res.status(404).end();
+  }
+  console.log(req.blog.toJSON());
+  await req.blog.destroy();
+  return res.status(204).end();
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (!req.blog) {
+    return res.status(404).end();
+  }
+  req.blog.likes = req.body.likes;
+  await req.blog.save();
+  console.log(req.blog.toJSON());
+  return res.json(req.blog);
 });
 
 module.exports = router;
