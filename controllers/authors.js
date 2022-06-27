65b0a838c6a2d632ed083f53ebc83263b072ab87
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-06-27 22:15:44 +0800

    Task 13.16.

---
+++ controllers/authors.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const sequelize = require('sequelize');
+
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      "author",
+      [sequelize.fn('COUNT', sequelize.col('id')), "blogs"],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+    group: 'author',
+    order: [
+      [sequelize.fn('SUM', sequelize.col('likes')), 'DESC'],
+    ],
+  });
+
+  console.log(JSON.stringify(authors, null, 2));
+  res.json(authors);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const authorsRouter = require('./controllers/authors')
 
 const { PORT } = require('./utils/config');
 const { connectToDatabase } = require('./utils/db');
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/authors', authorsRouter);
 
 const main = async () => {
   await connectToDatabase;
